aabe504f7b75201da9e71749de27804e57c94f67
alexburton7712 <alexburton7712@icloud.com>
2024-08-18 22:43:53 -0700

    Hello

--- TheGangMod-Forge copy/src/main/java/net/alexburton/gangmod/GangMod.java
+++ TheGangMod-Forge copy/src/main/java/net/alexburton/gangmod/GangMod.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
+//Hello There
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(GangMod.MOD_ID)
 public class GangMod
